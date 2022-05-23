a2450cc137fa2712aa7ce5fd4fe60bccc951273c
walterlbf <walter.lbf.f@gmail.com>
2022-05-22 23:38:17 -0400

    dados do laboratorio set e get

--- src/main/java/com/example/demo/model/Laboratorio.java
+++ src/main/java/com/example/demo/model/Laboratorio.java
@@ -1,5 +1,35 @@
 package com.example.demo.model;
 
-public class Laboratorio {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Laboratorio {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String nomePropriedade;
+
+  public Laboratorio(long id, String nomePropriedade) {
+    super();
+    this.id = id;
+    this.nomePropriedade = nomePropriedade;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getNomePropriedade() {
+    return nomePropriedade;
+  }
+
+  public void setNomePropriedade(String nomePropriedade) {
+    this.nomePropriedade = nomePropriedade;
+  }
 }
