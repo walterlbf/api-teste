fc3c003e5571839e863e9c458143f340dd3504b9
walterlbf <walter.lbf.f@gmail.com>
2022-05-23 00:37:35 -0400

    service do cadastro criado

--- src/main/java/com/example/demo/service/CadastroUsuarioService.java
+++ src/main/java/com/example/demo/service/CadastroUsuarioService.java
@@ -1,8 +1,21 @@
 package com.example.demo.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.example.demo.model.CadastroUsuario;
+import com.example.demo.repository.CadastroUsuarioRepository;
 
 @Service
 public class CadastroUsuarioService {
+  @Autowired
+  private CadastroUsuarioRepository repository;
+
+  public List<CadastroUsuario> FindAll() {
+
+    List<CadastroUsuario> usuarios = repository.findAll();
+
+    return usuarios;
+  }
 
 }
