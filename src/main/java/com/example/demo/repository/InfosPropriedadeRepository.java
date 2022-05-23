1c79adc41108b1d1096258a8260db418b5a34c91
walterlbf <walter.lbf.f@gmail.com>
2022-05-23 00:15:20 -0400

    repository refatorado

--- src/main/java/com/example/demo/repository/InfosPropriedadeRepository.java
+++ src/main/java/com/example/demo/repository/InfosPropriedadeRepository.java
@@ -2,7 +2,8 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.example.demo.model.InfosPropriedade;
 
 @Repository
-public interface InfosPropriedade extends JpaRepository<InfosPropriedade, Long> {
+public interface InfosPropriedadeRepository extends JpaRepository<InfosPropriedade, Long> {
 }
--- src/main/java/com/example/demo/repository/Laboratorio.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import javax.persistence.Entity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Entity
-public interface Laboratorio extends JpaRepository<Laboratorio, Long> {
-
-}
